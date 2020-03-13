60e52cdfdbe589f10923cb218df13f44c790a88b
angelo1018 <wagnzh@shanyingintl.com>
2020-03-13 18:03:34 +0800

    可以打包版本

--- collector/dbtoxls.py
+++ collector/dbtoxls.py
@@ -1,13 +1,12 @@
 # Pyhton 下用Django语法增删查改，注意创建空的数据库需要在终端或数据库软件中，表结构需要在models.py模块
-import os, django,pymysql, inspect,sys, datetime,time
-os.environ.setdefault ( "DJANGO_SETTINGS_MODULE" , "IBS.settings" )  # project_name 项目名称
+import os, django, pymysql, inspect, sys, datetime, time
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "IBS.settings")  # project_name 项目名称
 django.setup()
 
 from openpyxl import load_workbook , Workbook
 from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font,colors
 
 from collector import models,models_view
-
 
 
 # 思路历程：
@@ -19,7 +18,7 @@ from collector import models,models_view
 # print(models.Account.__dict__)
 # print(models.Account.__name__)
 # #取出表的中文名
-# print(models.Account._meta.verbose_name_plural)
+# print(models.Account._meta.verbose_name_plural)and 'Python' not in path_to_tcl
 
 # #取出字段的中文名
 # fielddic1={}
@@ -66,7 +65,7 @@ for name, obj in inspect.getmembers(models):
         # print(fields1)
         ws.append(fields1)
         ws.append(fields2)
-        ws.row_dimensions.group(1,1, hidden=True)
+        # ws.row_dimensions.group(1,1, hidden=True)
         # ws.sheet_state = 'hidden'
         # ws.protection.enable()
 for name,obj in inspect.getmembers(models_view):
@@ -81,7 +80,7 @@ for name,obj in inspect.getmembers(models_view):
         # print(fields1)
         ws.append(fields1)
         ws.append(fields2)
-        ws.row_dimensions.group(1,1, hidden=True)
+        # ws.row_dimensions.group(1,1, hidden=True)
         ws.sheet_state = 'hidden'
         ws.protection.enable()
  # 导出科目表
@@ -95,27 +94,27 @@ for obj in models.AccountType.objects.all().values_list():
     print(obj)
     ws.append(obj)
 # 导出公司表
-    for obj in models.Company.objects.all().values_list():
-        ws = wb['Company']
-        print(obj)
-        ws.append(obj)
+for obj in models.Company.objects.all().values_list():
+    ws = wb['Company']
+    print(obj)
+    ws.append(obj)
 # 导出版本表
-    for obj in models.Version.objects.all().values_list():
-        ws = wb['Version']
-        print(obj)
-        ws.append(obj)
+for obj in models.Version.objects.all().values_list():
+    ws = wb['Version']
+    print(obj)
+    ws.append(obj)
 
 # 导出币种
-    for obj in models.Currency.objects.all().values_list():
-        ws =wb['Currency']
-        print(obj)
-        ws.append(obj)
+for obj in models.Currency.objects.all().values_list():
+    ws =wb['Currency']
+    print(obj)
+    ws.append(obj)
 
 # 导出汇率表
-    for obj in models.CurrencyRate.objects.all().values_list():
-        ws = wb['CurrencyRate']
-        print(obj)
-        ws.append(obj)
+for obj in models.CurrencyRate.objects.all().values_list():
+    ws = wb['CurrencyRate']
+    print(obj)
+    ws.append(obj)
 
 ws = wb.active
 ws.title = 'Cover'
@@ -124,8 +123,22 @@ ws['A4'].font = Font(name='Arial', size=14, color=colors.RED, italic=True)
 ws['A5'] = '填报期间:'
 ws['A7'] = '版本号'
 # ws.sheet_state = 'hidden'
-ws.protection.enable()
-wb.save('FrpTemplateV1.0.xlsx')
+# ws.protection.enable()
+
+import os,tkinter as tk
+application_window = tk.Tk()
+application_window.withdraw() #隐藏消息框
+
+# Build a list of tuples for each file type the file dialog should display
+my_filetypes = [('xlsx files', '.xlsx')]
+from tkinter import filedialog
+# localpath = filedialog.asksaveasfile(mode = "w",parent = application_window,initialdir = os.getcwd(),title="Please select a location:",filetypes = my_filetypes)
+# localpath = filedialog.askdirectory()
+localpath = filedialog.asksaveasfilename(parent = application_window,initialdir = os.getcwd(),title="Please select a location:",filetypes = my_filetypes)
+
+
+
+wb.save(localpath)
 
 
 
